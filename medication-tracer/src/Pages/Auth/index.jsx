9bc0645c985fa8a886ccb19a8b79c2162ca008b8
Udeogu Alex <alexanderudeogu@gmail.com>
2024-05-29 21:38:38 +0100

    started the login initialization process

--- medication-tracer/src/Pages/Auth/index.jsx
+++ medication-tracer/src/Pages/Auth/index.jsx
@@ -1,51 +1,76 @@
-import React from 'react';
-import './auth-index.css'
-import cutout from "../../assets/sitting-5.svg"
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import './auth-index.css';
+import cutout from "../../assets/sitting-5.svg";
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const predefinedUser = {
+    username: "alexudeogu@gmail.com",
+    password: "JD2Audex",
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (username === predefinedUser.username && password === predefinedUser.password) {
+      navigate('/med-page');
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+
   return (
-
     <div className='body'>
-
-        <nav className='nav-2'>
-            <Link to="/" className='logo-1'> Medi<span className='logo-half'>Tracer</span></Link>
-            <ul>
-                <li>Don't have an account?</li>
-                <li><Link to="/sign-up"> <botton type="submit" className="item-container-1" > Sign Up </botton></Link></li>
-            </ul>
-        </nav>
+      <nav className='nav-2'>
+        <Link to="/" className='logo-1'> Medi<span className='logo-half'>Tracer</span></Link>
+        <ul>
+          <li>Don't have an account?</li>
+          <li><Link to="/sign-up"><botton type="button" className="item-container-1">Sign Up</botton></Link></li>
+        </ul>
+      </nav>
 
       <div className="login-page-1">
         <div className="login-container-1">
-          <form className="login-form-1">
+          <form className="login-form-1" onSubmit={handleLogin}>
             <h2 className="login-title-1">Log In</h2>
 
-
-
             <div className="login-form-group-1">
-              <label htmlFor="email" className="login-form-label-1">Username or Email:</label>
-              <input type="email" id="email" className="login-form-input-1" placeholder="Enter your username or email" />
+              <label htmlFor="username" className="login-form-label-1">Username or Email:</label>
+              <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+                id="username"
+                className="login-form-input-1"
+                placeholder="Enter your username or email"
+                required
+              />
             </div>
-
-
 
             <div className="login-form-group-1">
               <label htmlFor="password" className="login-form-label-1">Password:</label>
-              <input type='password' id="password" className="login-form-input-1" placeholder="Enter your password" />
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type='password'
+                id="password"
+                className="login-form-input-1"
+                placeholder="Enter your password"
+                required
+              />
             </div>
-
 
             <button type="submit" className="login-form-button-1">Log In</button>
             <div className="forgotpassword">
               <a href='#'>Forgot Password?</a>
             </div>
-
-            
           </form>
         </div>
         <div className="cutout-container-1">
-        <img src={cutout} alt="Tools" className="cutout-img-1" />
+          <img src={cutout} alt="Tools" className="cutout-img-1" />
         </div>
       </div>
     </div>
