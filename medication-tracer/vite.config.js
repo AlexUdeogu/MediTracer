ad18d538df55a196b812893c85e84008d1632494
Udeogu Alex <alexanderudeogu@gmail.com>
2024-07-17 12:11:20 +0100

    Last commit for Version 1

--- medication-tracer/vite.config.js
+++ medication-tracer/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteExternalsPlugin } from 'vite-plugin-externals';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/react-fontawesome'],
-    },
-  },
+  plugins: [
+    react(),
+    viteExternalsPlugin({
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      axios: 'axios',
+    }),
+  ],
   server: {
     host: true,
-    port: 3000,
+    port: 3000, // Adjust the port as needed
   },
 });
