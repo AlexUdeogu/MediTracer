4d7f3e42b7db52ab76ffce279ce53d262c5eea75
Udeogu Alex <alexanderudeogu@gmail.com>
2024-07-17 12:39:05 +0100

    Last commit for Version 1

--- medication-tracer/vite.config.js
+++ medication-tracer/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@fortawesome/react-fontawesome', 'framer-motion'],
+      external: ['@fortawesome/react-fontawesome', 'framer-motion', 'axios'],
     },
   },
   server: {
